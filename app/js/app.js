8e0061c749c29f2de9bed1af4e3d9cf54576576a
Denis Belocerkovec <belocerkovecden@gmail.com>
2021-10-20 18:00:35 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,50 @@
+class RunText {
+    constructor(obj) {
+        this.run_text__wrap = document.querySelector(obj.run_text__wrap);
+        this.run_text = document.querySelectorAll(obj.run_text);
+        this.i = true;
+        this.bias_transform = obj.bias_transform || 300;
+        this.debounce_time = 0;
+        this.run_text__wrap.addEventListener('mousemove', this.move.bind(this))
+    }
+
+    move () {
+        clearTimeout(this.debounce_time);
+        this.debounce_time = setTimeout(() => {
+            if (this.i) {
+                this.run_text.forEach((item, index, arr) => {
+                    if ((index % 2) !== 0) {
+                        item.style.transform = `translateX(${this.bias_transform}px)`
+                    } else {
+                        item.style.transform = `translateX(-${this.bias_transform}px)`
+                    }
+                });
+                this.i = false;
+            } else {
+                this.run_text.forEach((item, index, arr) => {
+                    if ((index % 2) !== 0) {
+                        item.style.transform = `translateX(-${this.bias_transform}px)`
+                    } else {
+                        item.style.transform = `translateX(${this.bias_transform}px)`
+                    }
+                });
+                this.i = true;
+            }
+        }, 100);
+    }
+}
+
+window.addEventListener('load', () => {
+    let obj = {
+        run_text__wrap: '.sg__container',
+        run_text: '.run-text',
+        bias_transform: 300
+    }
+    new RunText(obj);
+    let obj_dark = {
+        run_text__wrap: '.sg__container',
+        run_text: '.run-text-dark',
+        bias_transform: 350
+    }
+    new RunText(obj_dark);
+});
